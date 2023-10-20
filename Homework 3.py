b4b45caf22e761c17857b82bf7a3d37fb86d4b15
TuanMinh16702 <trangtuanminh87@gmail.com>
2023-10-20 22:56:11 +0700

    up

--- Homework 3.py
+++ Homework 3.py
@@ -1,33 +1,43 @@
-import cv2
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 
-Bin_file_path = "actontBinbin.sec"
+# Đọc hình ảnh nhị phân "actontBin.bin"
 
-actonBin = np.fromfile('binfile/actontBin.bin', dtype= np.uint8).reshape(256,256)
+image_data = np.fromfile('binfile/actontBin.bin', dtype=np.uint8, count=256*256)
+hinh_anh_nhap = image_data.reshape(256, 256)
 
+# Thiết kế mẫu "T"
+template = np.array([[1, 1, 1],
+                    [0, 1, 0],
+                    [0, 1, 0]], dtype=np.uint8)
 
+# Tính kích thước mẫu
+chieu_cao_mau, chieu_rong_mau = template.shape
 
+# Khởi tạo hình ảnh kết quả J
+hinh_anh_ket_qua = np.zeros_like(hinh_anh_nhap, dtype=np.float32)
 
+# Lặp qua hình ảnh đầu vào và tính giá trị độ tương quan tại mỗi điểm ảnh sử dụng mẫu
+for y in range(chieu_cao_mau // 2, 256 - chieu_cao_mau // 2):
+    for x in range(chieu_rong_mau // 2, 256 - chieu_rong_mau // 2):
+        khu_vuc_xung_quanh = hinh_anh_nhap[y - chieu_cao_mau // 2:y + chieu_cao_mau // 2 + 1,
+                                          x - chieu_rong_mau // 2:x + chieu_rong_mau // 2 + 1]
+        do_tuong_quan = np.sum(khu_vuc_xung_quanh * template)
+        hinh_anh_ket_qua[y, x] = do_tuong_quan
 
-result = cv2.matchTemplate(actonBin, template, cv2.TM_CCOEFF_NORMED)
+# Threshold để tạo hình ảnh nhị phân Jz
+nguong = np.max(hinh_anh_ket_qua) * 0.9  # Điều chỉnh ngưỡng theo nhu cầu
+Jz = np.where(hinh_anh_ket_qua > nguong, 255, 0).astype(np.uint8)
 
-
-threshold = 0.2  # Giá trị ngưỡng tùy chỉnh
-
-J2 = np.where(result >= threshold, 255, 0).astype(np.uint8)
-
-#Show ảnh gốc
+# Hiển thị cả hình ảnh gốc và hình ảnh kết quả
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
-plt.title('Ảnh gốc')
-plt.imshow(actonBin, cmap='gray')
-plt.axis('off')
+plt.imshow(hinh_anh_nhap, cmap='gray')
+plt.title("Hình ảnh gốc")
 
-#Show ảnh J2
 plt.subplot(1, 2, 2)
-plt.title('Kết quả')
-plt.imshow(J2, cmap='gray')
-plt.axis('off')
+plt.imshow(Jz, cmap='gray')
+plt.title("Hình ảnh sau khi tìm 'T'")
 
 plt.show()
